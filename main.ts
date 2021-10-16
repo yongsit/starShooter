63eab09a4ca55dfc32451dedb41a8b180eb3230f
Yongsit Meeprasert <ginosty@outlook.com>
2021-10-16 13:25:58 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,28 @@
-
+function rollPitch (pos: any[]) {
+    if (input.rotation(Rotation.Roll) > 20 && pos[0] < 4) {
+        pos[0] += 1
+    }
+    if (input.rotation(Rotation.Roll) < -20 && pos[0] > 0) {
+        pos[0] += -1
+    }
+    if (input.rotation(Rotation.Pitch) > 20 && pos[1] < 4) {
+        pos[1] += 1
+    }
+    if (input.rotation(Rotation.Pitch) < -20 && pos[1] > 0) {
+        pos[1] += -1
+    }
+}
+function moveDot(pos: any[]) {
+    led.unplot(pos[0], pos[1])
+    rollPitch(pos)
+    drawDot(pos)
+}
+function drawDot(pos: any[]) {
+    led.plot(pos[0], pos[1])
+}
+let position = [2, 2]
+drawDot(position)
+basic.forever(function () {
+    basic.pause(200)
+    moveDot(position)
+})
